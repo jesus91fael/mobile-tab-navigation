b52f8cf24b829abb786e7603e3ef31a88a4a6cd6
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-07-19 10:45:19 -0300

    chore: correção final

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import'./styles.css'
 
 const Home = () =>{
     return(
-        <img src='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'/>
+        <div className='home'><img src='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80'></img></div>
     )
 }
 
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -3,7 +3,7 @@ import'./styles.css'
 
 const Work = () =>{
     return(
-        <div className='work'><img src='https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'></img></div>
+        <div className='home'><img src='https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'></img></div>
     )
 }
 
